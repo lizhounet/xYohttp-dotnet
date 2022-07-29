7e3f563c1bff3f656ff7930d5f33446b5ff2c13e
士杰 余 <993359840@qq.com>
2022-07-29 23:39:23 +0800

    调整部分，预计明天结束

--- src/xYohttp-dotnet/Common/Constant/ApiFunctionConstant.cs
+++ src/xYohttp-dotnet/Common/Constant/ApiFunctionConstant.cs
@@ -14,5 +14,30 @@ namespace xYohttp_dotnet.Common.Constant
         /// 发送文字消息
         /// </summary>
         public const string SendTextMsg = "SendTextMsg";
+        /// <summary>
+        /// 发送文字消息_企业
+        /// </summary>
+        public const string SendTextMsgEnterprise = "SendTextMsgEnterprise";
+        /// <summary>
+        /// 发送群消息并艾特成员
+        /// </summary>
+        public const string SendGroupMsgAndAt = "SendGroupMsgAndAt";
+        /// <summary>
+        /// 发送群消息并艾特成员_企业
+        /// </summary>
+        public const string SendGroupMsgAndAtEnterprise = "SendGroupMsgAndAtEnterprise";
+        /// <summary>
+        /// 发送图片消息
+        /// </summary>
+        public const string SendImageMsg = "SendImageMsg";
+        /// <summary>
+        /// 发送图片消息_企业
+        /// </summary>
+        public const string SendImageMsgEnterprise = "SendImageMsgEnterprise";
+
+
+
+
+
     }
 }
--- src/xYohttp-dotnet/Http/XyoHttpApi.cs
+++ src/xYohttp-dotnet/Http/XyoHttpApi.cs
@@ -47,7 +47,66 @@ namespace xYohttp_dotnet.Http
             };
             return await PostAsync<dynamic>(body);
         }
-
+        /// <summary>
+        /// 发送文字消息_企业
+        /// </summary>
+        /// <returns></returns>
+        public async Task<dynamic> SendTextMsgEnterpriseAsync(string robotWxid, string toWxid, string msg)
+        {
+            var body = new
+            {
+                token = _token,
+                api = ApiFunctionConstant.SendTextMsgEnterprise,
+                robot_wxid = robotWxid,
+                to_wxid = toWxid,
+                msg
+            };
+            return await PostAsync<dynamic>(body);
+        }
+        /// <summary>
+        /// 发送群消息并艾特成员
+        /// </summary>
+        /// <param name="robotWxid">机器人ID</param>
+        /// <param name="groupWxid">群ID</param>
+        /// <param name="memberWxid">要艾特的群成员ID，艾特多人用英文逗号“,”</param>
+        /// <param name="memberName">要艾特的群成员昵称，可空 会自动读取</param>
+        /// <param name="msg">消息内容</param>
+        /// <returns></returns>
+        public async Task<dynamic> SendGroupMsgAndAtAsync(string robotWxid, string groupWxid,string memberWxid,string memberName, string msg)
+        {
+            var body = new
+            {
+                token = _token,
+                api = ApiFunctionConstant.SendGroupMsgAndAt,
+                robot_wxid = robotWxid,
+                group_wxid = groupWxid,
+                member_wxid = memberWxid,
+                member_name = memberName,
+                msg
+            };
+            return await PostAsync<dynamic>(body);
+        }
+        /// <summary>
+        /// 发送群消息并艾特成员
+        /// </summary>
+        /// <param name="robotWxid">机器人ID</param>
+        /// <param name="groupWxid">群ID</param>
+        /// <param name="memberWxid">要艾特的群成员ID，艾特多人用英文逗号“,”</param>
+        /// <param name="msg">消息内容</param>
+        /// <returns></returns>
+        public async Task<dynamic> SendGroupMsgAndAtEnterpriseAsync(string robotWxid, string groupWxid, string memberWxid,  string msg)
+        {
+            var body = new
+            {
+                token = _token,
+                api = ApiFunctionConstant.SendGroupMsgAndAtEnterprise,
+                robot_wxid = robotWxid,
+                group_wxid = groupWxid,
+                member_wxid = memberWxid,
+                msg
+            };
+            return await PostAsync<dynamic>(body);
+        }
         /// <summary>
         /// 发起请求
         /// </summary>
